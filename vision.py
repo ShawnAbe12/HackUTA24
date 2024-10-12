06175a1a5cdeeaf51e88754f7d9bc63c43c8e44c
devw45 <devin.wingfield45@gmail.com>
2024-10-12 16:21:09 -0500

    working code

--- vision.py
+++ vision.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(1) # 0 for default camera, 1 for external camera
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
@@ -13,6 +13,10 @@ cTime = 0
 
 while True:
     success, img = cap.read()
+    if not success:
+        print("Failed to capture image")
+        continue
+
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
     # print(results.multi_hand_landmarks)
